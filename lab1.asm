df5a26c4120dbbab66cede131fe74141218fb41f
Kirill <79408397+kirillqwerty@users.noreply.github.com>
2021-09-16 20:04:48 +0300

    Update lab1.asm

--- lab1.asm
+++ lab1.asm
@@ -1,10 +1,10 @@
 .model small
 .stack 100h
 .data
-a dw 6
-b dw 5
+a dw 8
+b dw 7
 c dw 5
-d dw 11
+d dw 5 
 
 .code
 .386
@@ -46,7 +46,7 @@ start:
         FirstResult:
         mov ax, a
         mov bx, b 
-        and ax, bx
+        and bx, ax
         jmp TheEnd
 
         SecondResult:
@@ -65,33 +65,78 @@ start:
         next5: mov ax, d
         next6: mov bx, ax
 
-        cmp ax, b   
+        xor ax, ax
+        cmp bx, a
+        je check1
+        jne continue1
+        check1:
+            add ax, 1
+        continue1:
+        cmp bx, b
+        je check2
+        jne continue2
+        check2:
+            add ax, 1
+        continue2: 
+        cmp bx, c
+        je check3
+        jne continue3
+        check3:
+            add ax, 1
+        continue3:
+        cmp bx, d
+        je check4
+        jne continue4
+        check4:
+            add ax, 1
+        continue4:
+
+        cmp ax, 2
+        jge Result
+        jl continue5
+
+        Result:
+            mov ax, bx
+            mul ax
+            jmp TheEnd
+
+        continue5:
+        mov ax, a   
         jg next7
         jle next8
-        next7: cmp bx, b
+        next7: cmp bx, a
         je next8
         jne next9
-        next9: mov ax, b
+        next9: mov ax, a
 
         next8:
-        cmp ax, c   
+        cmp ax, b   
         jg next10
         jle next11
-        next10: cmp bx, c
+        next10: cmp bx, b
         je next11
         jne next12
-        next12: mov ax, c
+        next12: mov ax, b
 
         next11:
-        cmp ax, d   
+        cmp ax, c   
         jg next13
         jle next14
-        next13: cmp bx, d
+        next13: cmp bx, c
         je next14
         jne next15
-        next15: mov ax, d
+        next15: mov ax, c
 
         next14:
+        cmp ax, d   
+        jg next16
+        jle next17
+        next16: cmp bx, d
+        je next17
+        jne next18
+        next18: mov ax, d
+
+        next17:
         mov dx, bx
         mov cx, ax
         mul dx
@@ -105,4 +150,4 @@ start:
 
 TheEnd:  
 int   21h
-end start   
+end start
